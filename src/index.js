5c67917a363c53aa6bfab51516b87b0b67bdf483
Robinee <yb6925@naver.com>
2023-08-11 22:55:47 +0900

    Add controllTodoForm for test

--- src/index.js
+++ src/index.js
@@ -1,54 +1,50 @@
 import './style.css';
 
-const showFormbtn = document.querySelector('.project-btn');
-const projectForm = document.querySelector('.project-form');
-const container = document.querySelector('.container');
-
 controllProjectForm();
 
 function controllProjectForm() {
   //This function is all about the project form
 
-  //**variables
+  //**variables */
+  const showFormbtn = document.querySelector('.project-btn');
+  const projectForm = document.querySelector('.project-form');
+  const container = document.querySelector('.projects');
   const addBtn = projectForm.querySelector('.add-btn');
   const cancelBtn = projectForm.querySelector('.cancel-btn');
   const input = projectForm.querySelector('#project-name');
 
   //**EventListener */
   showFormbtn.addEventListener('click', () => {
-    //1. show form --later do this in css (we will toggle class)
-    projectForm.style = 'display: block';
+    //showForm
+    projectForm.classList.add('show');
   });
 
   //2. click add btn
   addBtn.addEventListener('click', () => {
     addNewProject();
-    //hide the form after finish
     //clean form
     cleanFormInput();
   });
 
-  //3. click cancel btn
-  cancelBtn.addEventListener('click', () => {
-    //clean form
-    cleanFormInput();
-    //hide the form --later do this in css (we will toggle class)
-    projectForm.style = 'display: none';
-  });
-
-  //4. Press enter to submit the form instead of clicking add btn
+  //3. Press enter to submit the form instead of clicking add btn
   projectForm.addEventListener('submit', (e) => {
     e.preventDefault();
     addNewProject();
     //clean form
     cleanFormInput();
-    //hide the form after finish
+  });
+
+  //4. click cancel btn
+  cancelBtn.addEventListener('click', () => {
+    //clean form
+    cleanFormInput();
+    //hide the form --later do this in css (we will toggle class)
+    projectForm.classList.remove('show');
   });
 
   function addNewProject() {
     if (input.value.length === 0) {
       //if form is empty
-      //alert "Please write down a new project name"
       alert('Please write down a new project name');
       return;
     } else {
@@ -58,7 +54,9 @@ function controllProjectForm() {
       li.textContent = input.value;
       container.append(li);
 
-      projectForm.style = 'display: none';
+      li.addEventListener('click', controllTodoForm);
+
+      projectForm.classList.remove('show');
       return;
     }
   }
@@ -67,4 +65,10 @@ function controllProjectForm() {
     //clean the input
     input.value = '';
   }
+}
+
+function controllTodoForm(e) {
+  const todoForm = document.querySelector('.todo-form');
+  console.log('Show the form to make a todo');
+  todoForm.classList.add('show');
 }

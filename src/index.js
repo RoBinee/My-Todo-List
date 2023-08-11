1c42bce6e8246da4ec10e3425b3e7b79fa5081d3
Robinee <yb6925@naver.com>
2023-08-11 16:54:09 +0900

    Problem was duplicated eventListener, solved this problem by arranging  the code order

--- src/index.js
+++ src/index.js
@@ -1,47 +1,62 @@
-const btn = document.querySelector('.project-btn');
+const showFormbtn = document.querySelector('.project-btn');
 const projectForm = document.querySelector('.project-form');
 const container = document.querySelector('.container');
 
-btn.addEventListener('click', controllProjectForm);
+controllProjectForm();
 
 function controllProjectForm() {
+  //This function is all about the project form
+
   //**variables
   const addBtn = projectForm.querySelector('.add-btn');
   const cancelBtn = projectForm.querySelector('.cancel-btn');
   const input = projectForm.querySelector('#project-name');
 
-  //1. show form --later do this in css (we will toggle class)
-  projectForm.style = 'display: block';
+  //**EventListener */
+  showFormbtn.addEventListener('click', () => {
+    //1. show form --later do this in css (we will toggle class)
+    projectForm.style = 'display: block';
+  });
 
   //2. click add btn
-  addBtn.addEventListener('click', controllInput);
+  addBtn.addEventListener('click', () => {
+    addNewProject();
+  });
 
-  function controllInput() {
+  //3. click cancel btn
+  //hide form --later do this in css (we will toggle class)
+  cancelBtn.addEventListener('click', () => {
+    cleanFormInput(); //hide the form after finish
+    projectForm.style = 'display: none';
+  });
+
+  //4. Press enter to submit the form instead of clicking add btn
+  projectForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addNewProject();
+  });
+
+  function addNewProject() {
     if (input.value.length === 0) {
       //if form is empty
       //alert "Please write down a new project name"
       alert('Please write down a new project name');
+      return;
     } else {
       //if form is filled
       //show the content on the screen first
       const h1Test = document.createElement('h1');
       h1Test.textContent = input.value;
       container.append(h1Test);
-
-      initializeForm();
+      cleanFormInput();
+      //hide the form after finish
+      projectForm.style = 'display: none';
+      return;
     }
   }
 
-  //3. click cancel btn
-  //hide form --later do this in css (we will toggle class)
-  cancelBtn.addEventListener('click', initializeForm);
-
-  function initializeForm() {
+  function cleanFormInput() {
     //clean the input
     input.value = '';
-    //hide the form after finish
-    projectForm.style = 'display: none';
-    //remove eventListener
-    addBtn.removeEventListener('click', controllInput);
   }
 }

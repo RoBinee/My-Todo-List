ff44a68e63d8645cf244da5a385d67b5f9cbce96
Robinee <yb6925@naver.com>
2023-08-15 13:55:57 +0900

    Extract data and Project factory function from controllProject.js

---
+++ src/data.js
@@ -0,0 +1,20 @@
+let data = [{ id: '000', title: 'default project1', todos: [] }];
+
+function Project(id, title) {
+  return { id, title, todos: [] };
+}
+
+function addProjectToData(id, value) {
+  const projectObj = Project(id, value);
+  data.push(projectObj);
+}
+
+function removeProjectFromData(projectId) {
+  data = data.filter((projectObj) => {
+    if (projectObj.id !== projectId) {
+      return projectObj;
+    }
+  });
+}
+
+export { data, addProjectToData, removeProjectFromData };

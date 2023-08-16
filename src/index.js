5431be25fd968390ac4ef852ccff945154499ea0
Robinee <yb6925@naver.com>
2023-08-16 18:26:41 +0900

    build temporary code for displaying default project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import './style.css';
 import controllProjectForm from './controllProjectForm.js';
+import { data } from './data.js';
+import controllProject from './controllProject.js';
+
+if (data.length != 0) {
+  //display default project
+  data.forEach((item) => {
+    const { id, title } = item;
+    controllProject.addNewProject(title, id);
+  });
+}
 
 controllProjectForm();
